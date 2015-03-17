7a11da28453ccff33603dec48e914f8edcf34e3d
Xicks <schick159@hotmail.com>
2015-03-17 19:45:08 -0300

    Update ReverseClient.java

--- ReverseClient.java
+++ ReverseClient.java
@@ -3,30 +3,34 @@ import java.net.*;
 import java.util.*;
 /**
  * Author: Leonardo Schick
- * Data: 17/03/2015
+ * Date: 03/17/2015
  * Classe: ReverseClient
- * Esta classe abre uma conexão vai socket com o servidor. Ela lê um texto do usuário,
- * manda para o servidor e mostra a resposta.
+ * This class opens a connection with the server using a socket.
+ * Reads the input from the user, sends it to the server and shows the server's response.
  */
  
 public class ReverseClient {
 	
-	//************************************main()**********************************************//
+    //************************************main()**********************************************//
     public static void main(String[] args)
-	{
-       Socket s = null;  
+    {
+       	Socket s = null;  
         OutputStream os = null;
-		InputStream in = null;
+	InputStream in = null;
         BufferedReader reader = null;
         PrintWriter pw = null;
         try{
             try{
-				String computerName = InetAddress.getLocalHost().getHostName();
-				s = new Socket(computerName,16001);
-				os = s.getOutputStream();
-				in = s.getInputStream();
-				reader = new BufferedReader(new InputStreamReader(in));
-				pw = new PrintWriter(os,true);
+            		//Gets the machine name
+			String computerName = InetAddress.getLocalHost().getHostName();
+			//Creates a socket using the port given by argument
+			s = new Socket(computerName,16001);
+			os = s.getOutputStream();
+			in = s.getInputStream();
+			//Object responsible to read from the server
+			reader = new BufferedReader(new InputStreamReader(in));
+			//Object responsible to writes to the server
+			pw = new PrintWriter(os,true);
             }catch(UnknownHostException e)
             {
                 System.err.println("Trying to connect to unknown host: " + e);
@@ -37,19 +41,19 @@ public class ReverseClient {
         if (s != null && os != null && reader != null && pw != null) {
             try {
                 Scanner sc = new Scanner(System.in);
-				String responseLine = reader.readLine();
-				do{
-					System.out.println(responseLine);
-					String str = sc.nextLine();
-					pw.println(str);
-				}while((responseLine = reader.readLine()) != null);
+			String responseLine = reader.readLine();
+			do{
+				System.out.println(responseLine);
+				String str = sc.nextLine();
+				pw.println(str);
+			}while((responseLine = reader.readLine()) != null);
                 os.close();
                 reader.close();
                 s.close();   
             } catch (IOException e) {
-				System.err.println(e);
+		System.err.println(e);
             }
         }
-	}		
+    }		
 }
 
