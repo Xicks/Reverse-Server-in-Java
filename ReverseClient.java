220989d99065501da3d7779d0aef4d218a6f6009
Xicks <schick159@hotmail.com>
2015-03-24 17:05:36 -0300

    Update ReverseClient.java

--- ReverseClient.java
+++ ReverseClient.java
@@ -16,42 +16,42 @@ public class ReverseClient {
     {
        	Socket s = null;  
         OutputStream os = null;
-	InputStream in = null;
+		InputStream in = null;
         BufferedReader reader = null;
         PrintWriter pw = null;
         try{
             try{
-            		//Gets the machine name
-			String computerName = InetAddress.getLocalHost().getHostName();
-			//Creates a socket using the port given by argument
-			s = new Socket(computerName,16001);
-			os = s.getOutputStream();
-			in = s.getInputStream();
-			//Object responsible to read from the server
-			reader = new BufferedReader(new InputStreamReader(in));
-			//Object responsible to writes to the server
-			pw = new PrintWriter(os,true);
-            }catch(UnknownHostException e)
-            {
-                System.err.println("Trying to connect to unknown host: " + e);
-            }
+            	//Gets the machine name
+				String computerName = InetAddress.getLocalHost().getHostName();
+				//Creates a socket using the port given by argument
+				s = new Socket(computerName,16001);
+				os = s.getOutputStream();
+				in = s.getInputStream();
+				//Object responsible to read from the server
+				reader = new BufferedReader(new InputStreamReader(in));
+				//Object responsible to writes to the server
+				pw = new PrintWriter(os,true);
+            }catch(UnknownHostException e){
+	            System.err.println("Trying to connect to unknown host: " + e);
+	        }
         }catch(IOException e){
             System.err.println(e);
         }   
         if (s != null && os != null && reader != null && pw != null) {
             try {
-                Scanner sc = new Scanner(System.in);
-			String responseLine = reader.readLine();
-			do{
-				System.out.println(responseLine);
-				String str = sc.nextLine();
-				pw.println(str);
-			}while((responseLine = reader.readLine()) != null);
+            	Scanner sc = new Scanner(System.in);
+				String responseLine = reader.readLine();
+				do{
+					System.out.println(responseLine);
+					String str = sc.nextLine();
+					pw.println(str);
+				}while((responseLine = reader.readLine()) != null);
                 os.close();
                 reader.close();
-                s.close();   
+                s.close(); 
+                pw.close();
             } catch (IOException e) {
-		System.err.println(e);
+			System.err.println(e);
             }
         }
     }		
